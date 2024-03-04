45734106d930ca6d52430882ad0f595b11a16634
mjkim <minji.k@lgcns.com>
2024-03-04 18:05:55 +0900

    다국어 설정

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,21 +5,66 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Frontier',
+			// Set English as the default language for this site.
+			defaultLocale: 'root',
+			locales: {
+				// Korean docs in `src/content/docs/ko/`
+				root: {
+					label: '한국어',
+					lang: 'ko-KR',
+				},
+				// English docs in `src/content/docs/en/`
+				en: {
+					label: 'English',
+				},
+			},
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
+					label: '시작하기',
+					translations: {
+						en: 'Getting Started',
+					},
+					autogenerate: { directory: 'getting-started' },
+					// items: [
+					// 	// Each item here is one entry in the navigation menu.
+					// 	{ label: 'Example Guide', link: '/guides/example/' },
+					//
+					// ],
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: '핵심 개념',
+					translations: {
+						en: 'Core Concepts',
+					},
+					autogenerate: { directory: 'concepts' },
+					// items: [
+					// 	// Each item here is one entry in the navigation menu.
+					// 	{ label: 'Example Guide', link: '/guides/example/' },
+					//
+					// ],
+				},
+				{
+					label: '튜토리얼',
+					translations: {
+						en: 'Tutorial',
+					},
+					autogenerate: { directory: 'tutorial' },
+					// items: [
+					// 	// Each item here is one entry in the navigation menu.
+					// 	{ label: 'Example Guide', link: '/guides/example/' },
+					//
+					// ],
+				},
+				{
+					label: '참고',
+					translations: {
+						en: 'References',
+					},
+					autogenerate: { directory: 'references' },
 				},
 			],
 		}),
