0b3e8ef2abfdd543756c4642b2d379a19fc00afb
mjkim <minji.k@lgcns.com>
2024-04-05 16:13:09 +0900

    배포를 위한 url 설정 값 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site:' https://lgcns-build-center.github.io/',
 	integrations: [
 		starlight({
 			title: 'Frontier',
